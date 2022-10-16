454225b5f9ae24095933b2270e624d0f99da004d
stickhero23 <stickhero23@gmail.com>
2022-10-15 20:46:35 -0700

    Adding 101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main- Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	printf("\"and that piece of art is useful - Dora Korpar, 2015-10-19");
+	return (1);
 }
