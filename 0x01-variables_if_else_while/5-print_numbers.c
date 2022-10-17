c206da4e931fed82027edac164756342735be26b
stickhero23 <stickhero23@gmail.com>
2022-10-17 15:40:13 -0700

    Adding 5-print_numbers script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print the numbers 0 to 9.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int j = '0';
+
+	while (j <= 9)
+	{
+		printf("%i", j);
+		j++;
+	}
+	putchar('\n');
+
+	return (0);
+}
