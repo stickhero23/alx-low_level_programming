30eb2bc4508acff0230bb77fb1ed21bbdbe61bb4
stickhero23 <stickhero23@gmail.com>
2022-10-17 04:58:18 -0700

    Adding 2-print_alphabet script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print the letters in lower case.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char 2_alphabet = a;
+	while (2_alphabet <= 'z')
+	{
+		putchar ("%c", 2_alphabet);
+		2_alphabet++;
+	}
+	return (0);
+}
