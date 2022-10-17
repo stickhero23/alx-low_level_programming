fa0cec73aa7cd9e5c3e710b9a6af82544e67d50d
stickhero23 <stickhero23@gmail.com>
2022-10-17 01:54:18 -0700

    Adding 1-last_digit.c script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,10 +24,10 @@ int main(void)
 	}
 	else if (m == 0)
 	{
-		printf("Last digit of %i is %i and is less tha 5\n", n, m);
+		printf("Last digit of %i is %i and is 0\n", n, m);
 	}
 	else
-		printf("Last digit of %i is %i and is less than 6 and not zero\n", n, m);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
 	return (0);
 }
 
