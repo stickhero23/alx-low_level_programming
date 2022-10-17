0766631de8c32544fb75554e4f8606bdd7951ef1
stickhero23 <stickhero23@gmail.com>
2022-10-17 01:38:21 -0700

    Adding 1-last_digit script

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * The program assigns a random number to n and checks if negative,
+ * positive or 0
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+
+	if (m > 5)
+	{
+		printf("The last digit of %i is %m and is geater than 5\n", n, m);
+	}
+	else if (m == 0)
+	{
+		printf("The last digit of %i is %m and is less tha 5\n", n, m);
+	}
+	else
+		printf("The last digit of %i is %m and is less than 6 and not zero\n", n, m);
+	return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 The scripte 0-positive_or_negative prints whether a number is negative, positive or zero
+The 1-last_digit script finds the last digit of a number given
