c012ed2e21a2e1fda801197549551976d619897c
stickhero23 <stickhero23@gmail.com>
2022-10-20 09:07:37 -0700

    Adding 2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest number of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && b >= c)
+	{
+		largest = a;
+		printf("%i is the largest number", a);
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+		printf("%i is the largest number", b);
+	}
+	else
+	{
+		largest = c;
+		printf("%i is the largest number", c);
+	}
+
+	return (largest)
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,2 +1,3 @@
 The 0-main.c script tests for output when i is 0
 1-main.c checks for infinite loop bug
+Adding 2-largest_number.c
