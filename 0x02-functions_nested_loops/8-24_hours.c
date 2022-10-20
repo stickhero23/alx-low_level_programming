67df0f1fe51eb35a3edbe476ac259b011b614258
stickhero23 <stickhero23@gmail.com>
2022-10-19 18:28:06 -0700

    Adding 9=8-24_hours.c script

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point, prints every minute of the day.
+ *
+ * Returns: Always 0, Success.
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int mins = 0;
+
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (mins / 10));
+			_putchar('0' + (mins % 10));
+			mins++;
+		}
+		mins = 0;
+		hours++;
+	}
+}
