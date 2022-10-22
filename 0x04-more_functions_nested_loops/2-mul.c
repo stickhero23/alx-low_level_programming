a9583045d555cf14dc963fb6ae380dfd4c175aab
stickhero23 <stickhero23@gmail.com>
2022-10-22 04:51:00 -0700

    Adding 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: int type number
+ * @b: int type number.
+ * Return: returns the product of the two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 This **README FILE** for 0x04-more_functions_nested_loops assignments
 Checks for a digit
+The mult.c function multiplies an integer
