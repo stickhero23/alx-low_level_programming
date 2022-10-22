bef12cf7863e2b7571192ff73c41d7589e1e1b9d
stickhero23 <stickhero23@gmail.com>
2022-10-22 05:14:59 -0700

    Adding 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * return: Always 0, success.
+ */
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
