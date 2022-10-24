81be06b5421d6f358c6d795190d747daa0f6d660
stickhero23 <stickhero23@gmail.com>
2022-10-23 20:16:51 -0700

    Adding 5-more_numbers.c solution

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point, prints 0 to 14 ten times
+ * Return: Always 0, success.
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 10)
+				putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
