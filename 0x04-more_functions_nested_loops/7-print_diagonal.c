40ba4f1c6dff3677cc5b75ddf9376e4cfaaff16f
stickhero23 <stickhero23@gmail.com>
2022-10-23 23:02:25 -0700

    Adding 7 print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line on terminal.
+ * @n: int type number.
+ * Description: can only use _putchar.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@ The mult.c function multiplies an integer
 The solution 4-print_most_numbers.c prints 0 to 9, except 2 and 4
 a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
 The script 6-print_line.c draws a straight line
+print_diagonal draws a diagonal line on the terminal
