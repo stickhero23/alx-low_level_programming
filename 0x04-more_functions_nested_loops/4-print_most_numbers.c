f03f1d58492db2dc3886d638b365a9e1c09a074f
stickhero23 <stickhero23@gmail.com>
2022-10-24 17:49:45 -0700

    Adding print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	char c = 0;
+	int c;
 
-	while (c <= '9')
+	c = 0;
+	while (c <= 9)
 	{
-		if (c != '2' && c != '4')
-		{
+		if (c != 2 && c != 4)
 			_putchar(c);
-		}
 		c++;
 	}
 
