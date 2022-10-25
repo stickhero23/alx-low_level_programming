07d4c47f177d839819ac5f027999886629bb3188
stickhero23 <stickhero23@gmail.com>
2022-10-24 18:05:17 -0700

    Adding changes to 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@ void more_numbers(void)
 	int i;
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c < 10; c++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = 0; i < 15; i++)
 		{
-			if (i / 10 > 10)
+			if (i / 10 >= 10)
 			{
-				_putchar((i / 10) + '0');
+				_putchar((i / 10) + 48);
 			}
-			_putchar((i % 10) + '0');
+			_putchar((i % 10) + 48);
 		}
 		_putchar('\n');
 	}
