9cf0da5061685d2f74dbe8a40045b844eb9927a8
stickhero23 <stickhero23@gmail.com>
2022-10-29 13:37:32 -0700

    Adding task 0 solution

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,17 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i + j] = src[j];
-		i++;
-	}
+	i = 0;
+	j = 0;
 
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (0);
 }
