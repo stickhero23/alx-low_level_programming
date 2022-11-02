6c85caf152bb2a90a670ab85be125765704ad6ac
stickhero23 <stickhero23@gmail.com>
2022-11-02 10:23:00 -0700

    Adding3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to type char
+ * @accept: type char substring
+ * Return: number of bytes s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	i = 0;
+	k = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (k);
+}
