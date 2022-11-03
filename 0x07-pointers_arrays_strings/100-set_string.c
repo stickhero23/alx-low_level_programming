3090001f960584159204519e61053d8352101be9
stickhero23 <stickhero23@gmail.com>
2022-11-02 22:49:14 -0700

    Adding 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a chart
+ * @s: pointer to type char
+ * @to: pointer to type char
+ * Return: Set char value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
