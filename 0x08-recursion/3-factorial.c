b1ba07f84398406665de6cbbe55a593e9b1da2f5
stickhero23 <stickhero23@gmail.com>
2022-11-04 13:14:01 -0700

    Adding 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a number
+ * @n: integer type
+ * Return: returns the factorial of a number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 0-puts_recurssion.c prints a string
 1-print_rev_recursion.c prints a string in reverse
 2-strlen_recursion.c finds length of a string
+3-factorial.c finds factorial of n
