ca93345776ea5b39cc7e483c4debae8dbe4ac01f
stickhero23 <stickhero23@gmail.com>
2022-11-04 17:55:51 -0700

    Adding is palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,5 @@ int is_prime(int n, int num);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int get_len(char *s);
-int my _palindrome(char *s, int l);
+int my_palindrome(char *s, int l);
 #endif
