8f0ee326140ea1baf6530b1021ea38224c437441
stickhero23 <stickhero23@gmail.com>
2022-11-08 14:06:26 -0800

    Adding mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *main - entry point to print multiplication.
- *@argc: number of arguments
- *@argv: array of string
- *Return: Always 0, success
+ * main - entry point to print multiplication
+ * @argc: number of arguments
+ * @argv: array of arguments to command line
+ * Return: 0 if success and 1 is failure.
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[]);
 {
+	int mult;
 
-	if (argc != 3)
+	if (argc == 3)
+	{
+		mult = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", mult);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
 }
