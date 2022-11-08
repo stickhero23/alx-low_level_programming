e1dd35f44e4a212d3a3da915e259a19161e3da04
stickhero23 <stickhero23@gmail.com>
2022-11-08 14:12:00 -0800

    Adding 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,11 @@
  */
 int main(int argc, char *argv[]);
 {
-	int mult;
-
-	if (argc == 3)
-	{
-		mult = atoi(argv[1] * atoi(argv[2]);
-		printf("%d\n", mult);
-		return (0);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
