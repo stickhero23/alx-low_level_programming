1b0de5c5aff4e16324abc8f241a051e5a336b378
stickhero23 <stickhero23@gmail.com>
2022-11-07 18:11:46 -0800

    Adding Task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * @argc: number of arguments
+ * @argv: array of strings of arguments.
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
