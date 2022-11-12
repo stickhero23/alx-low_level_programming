b4ffe1225d9f7adde69a5d650c65840ddb15f87e
stickhero23 <stickhero23@gmail.com>
2022-11-12 00:25:05 +0000

    Adding 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,8 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	length = _strlen(s1) + k + 1; /* include +1 for null pointer */
 
 	p = malloc(sizeof(*p) * length);
-	for (p == NULL)
+	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++) /* concatenate */
 		p[i] = s1[i];
 	for (j = 0; j < k; j++)
