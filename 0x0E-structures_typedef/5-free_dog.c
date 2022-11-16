40da0ab9eddee68f78146d10564cc36bf5179bde
stickhero23 <stickhero23@gmail.com>
2022-11-16 16:24:14 +0000

    Adding 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: pointer type struct
+ * Return: Always 0, success.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
