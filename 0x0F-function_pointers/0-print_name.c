1f3db5e3ccc0a5cb5889d13bd11f59ce59f58ebb
stickhero23 <stickhero23@gmail.com>
2022-11-18 22:10:22 +0000

    Adding 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_name - to execute the function as a parameter
+ * @name: the paramenter for the function pointer
+ * @f: function to execute
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,5 @@
+#ifdef MAIN_H
+#define MAIN_H
+void print_name(char *name, void (*f)(char *));
+#endif
+
