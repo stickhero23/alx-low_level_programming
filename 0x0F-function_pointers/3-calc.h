a1666e9513d3fd9c06d42428fd6692a881ed6706
stickhero23 <stickhero23@gmail.com>
2022-11-19 00:43:36 +0000

    Adding task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -18,5 +18,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
-if (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
 #endif
--- 0x0F-function_pointers/get_op_func.c
+++ 0x0F-function_pointers/get_op_func.c
@@ -8,6 +8,8 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
