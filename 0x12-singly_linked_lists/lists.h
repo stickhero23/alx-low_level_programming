02354d4bffede77dfaf33294a75309fac1207986
stickhero23 <stickhero23@gmail.com>
2022-12-01 01:03:50 +0000

    Changing lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+/**
+ * struct singly_linked - singly linked list
+ * @str: str
+ * @len: length of string
+ * @next: pointer to the next node
+ */
+typedef struct singly_linked
+{
+	char *str;
+	unsigned int len;
+	struct singly_linked *next;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
