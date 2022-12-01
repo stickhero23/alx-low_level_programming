5efa1d2c986e7f8ecafc5cfa76e7044579ae6ac3
stickhero23 <stickhero23@gmail.com>
2022-12-01 00:55:12 +0000

    Adding task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all elements in list_t
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *n;
+	size_t c;
+
+	c = 0;
+	n = h;
+	while (n != NULL)
+	{
+		printf("[%d] %s\n", n->len, n->str);
+		n = n->next;
+		c++;
+	}
+	return (c);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This README file contains descriptions for assignments in 0x12-singly_linked_lists
