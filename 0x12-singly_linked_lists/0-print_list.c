679c43aec9697097637a2155e6f7901caf84703b
stickhero23 <stickhero23@gmail.com>
2022-12-01 01:09:35 +0000

    Fixed 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,10 @@ size_t print_list(const list_t *h)
 	n = h;
 	while (n != NULL)
 	{
-		printf("[%d] %s\n", n->len, n->str);
+		if (n->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", n->len, n->str);
 		n = n->next;
 		c++;
 	}
