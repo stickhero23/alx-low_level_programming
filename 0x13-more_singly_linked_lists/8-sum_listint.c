baaed87ca467dbdffc293b4cc7a520f86181725b
stickhero23 <stickhero23@gmail.com>
2022-12-02 02:02:09 +0000

    Add 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - returns sum of all elements
+ * @head: list
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	for (; temp != NULL;)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
