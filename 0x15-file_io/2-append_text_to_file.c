7119ef5921c9b34aae54cb1198d8dbdc058cab17
stickhero23 <stickhero23@gmail.com>
2022-12-09 00:46:15 +0000

    Adding 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	return (1);
 }
 /**
- * _strlen - find length of string 
+ * _strlen - find length of string
  * @str: string
  * Return: length of string
  */
