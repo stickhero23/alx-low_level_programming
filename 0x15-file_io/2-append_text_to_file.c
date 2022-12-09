f630c72e41f99092800abbb8712cbd76d78ee181
stickhero23 <stickhero23@gmail.com>
2022-12-09 00:40:47 +0000

    Adding 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,5 +8,26 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t 
+	int r_write;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	r_write = write(fd, text_content, _strlen(text_content));
+	if (r_write == -1 || r_write != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
