2f8cb01d6af7e707bb5b4e5983cd65600bfb7f84
stickhero23 <stickhero23@gmail.com>
2023-01-14 01:01:13 +0000

    adding fixes on tasks

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t list_count = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		list_count++;
+		cont++;
 	}
-	return (list_count);
+	return (cont);
 }
