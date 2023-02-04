a75c0417b2568631a13c93af49744244d826971a
stickhero23 <stickhero23@gmail.com>
2023-02-04 01:20:05 +0000

    Adding task 0

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,10 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-#include <stdio.h>
-#include <string.h>
+#ifndef _HASH_TABLES
+#define _HASH_TABLES
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
- * struct hash_node_s - node of a hash table
+ * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
@@ -19,23 +19,24 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- * typedef struct hash_table_s - hash table data structure
+ * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining Collision handling
+ * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-hash_tabel_t *hash_table_create(unsigned long int *size);
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+#endif /* #ifndef _HASH_TABLES */
 
