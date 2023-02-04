b33afc00c5fb9eab76b3b5043ca364c55b2e4260
stickhero23 <stickhero23@gmail.com>
2023-02-04 05:57:54 +0000

    Adding task 3-hash_table_set.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,20 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *htb;
+	hash_table_t *ht;
 	hash_node_t **array;
 	unsigned long int i = 0;
 
 	if (size == 0)
 		return (NULL);
 
-	htb = malloc(sizeof(hash_table_t));
-	if (htb == 0)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == 0)
 		return (NULL);
 	array = malloc(sizeof(*array) * size);
 	if (array == 0)
 	{
-		free(htb);
+		free(ht);
 		return (NULL);
 	}
 	while (i < size)
@@ -29,8 +29,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		i++;
 	}
 
-	htb->size = size;
-	htb->array = array;
+	ht->size = size;
+	ht->array = array;
 
-	return (htb);
+	return (ht);
 }
