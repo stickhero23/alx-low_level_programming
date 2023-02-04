3dac82ef8d80bfd0b74667973c640d1aed03bf5d
stickhero23 <stickhero23@gmail.com>
2023-02-04 03:15:31 +0000

    Adding task djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,7 +4,7 @@
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
  *
- * Return: hash value
+ * Return: Returns the hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
