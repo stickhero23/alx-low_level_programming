d58f92d3f01234dda390643f4ea918387e1f0a55
Stickhero23 <francisnyamache296@gmail.com>
2023-05-11 02:51:35 +0000

    fix 1-binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,5 @@
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t t, size_t size);
 #endif
